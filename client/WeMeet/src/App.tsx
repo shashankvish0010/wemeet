8556486e5bb8a1c14b15bc561e33a6bc89191dac
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-01-22 20:59:17 +0530

    Added: Routes for meeting room

--- client/WeMeet/src/App.tsx
+++ client/WeMeet/src/App.tsx
@@ -7,10 +7,10 @@ import Login from './pages/Login';
 import OtpVerification from './pages/OTP';
 import Events from './pages/Events';
 import Booking from './pages/Booking';
-import Meet from './pages/Meet';
 import Profile from './pages/Profile';
 import PaySuccess from './pages/PaySuccess';
 import PayCancel from './pages/PayCancel';
+import Meeting from './pages/Meeting';
 
 const App: React.FC = () => {
   return (
@@ -23,7 +23,7 @@ const App: React.FC = () => {
         <Route path='/otp/verification/:id' element={<OtpVerification />} />
         <Route path='/add/event/:id' element={<Events />} />
         <Route path='/book/:duration/:id' element={<Booking />} />
-        <Route path='/meeting/:id' element={<Meet/>} />
+        <Route path='/meeting/:id' element={<Meeting/>} />
         <Route path='/profile' element={<Profile/>} />
         <Route path='/success' element={<PaySuccess/>} />
         <Route path='/cancel' element={<PayCancel/>} />
