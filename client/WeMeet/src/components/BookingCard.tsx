df871c8043acb5ec737d41d1b779bd5fbcbdd5bd
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-03-07 20:54:07 +0530

    Changes: Style

--- client/WeMeet/src/components/BookingCard.tsx
+++ client/WeMeet/src/components/BookingCard.tsx
@@ -12,12 +12,12 @@ interface CardType {
 
 const BookingCard: React.FC<CardType> = (props: CardType) => {    
     return (
-        <div className='bg-slate-100 h-[25vh] md:w-[45vw] w-[80vw] md:p-3 p-2 shadow-2xl rounded-xl flex flex-row md:gap-3 gap-2'>
-            <div className='h-[100%] w-[12dvw] p-2 flex flex-col items-center justify-evenly'>
+        <div className='bg-slate-100 border-2 md:h-[25vh] h-[20vh] md:w-[45vw] w-[80vw] p-3 shadow-xl rounded-xl flex flex-row gap-3'>
+            <div className='h-[100%] w-[12vw] p-2 flex flex-col items-center justify-evenly'>
                 <span><p className='text-base p-1 font-semibold uppercase'>{`${props.month}`.slice(0,3)}</p></span>
                 <span><p className='text-2xl bg-orange-400 rounded-full px-2 py-1 text-center font-semibold'>{props.date}</p></span>
             </div>
-            <span className='w-[0.5rem] h-[100%] rounded bg-orange-400'></span>
+            <span className='w-[0.3rem] md:h-[100%] rounded bg-orange-400'></span>
             <div className='h-[100%] w-max flex flex-row gap-3 p-2 items-center'>
                 <div className='h-[100%] flex flex-col justify-around gap-4'>
                     <span className='flex flex-row gap-1 items-center md:text-base text-sm'>
@@ -31,7 +31,8 @@ const BookingCard: React.FC<CardType> = (props: CardType) => {
                 </div>
                 <div className='h-[100%] w-[100%] flex flex-col p-2 justify-around gap-4'>
                     <span>
-                        <p className='md:text-base text-sm'>{props.description.slice(0,80)}</p>
+                        <p className='md:block hidden text-base'>{props.description.slice(0,80)}</p>
+                        <p className='block md:hidden text-sm'>{props.description.slice(0,30)}...</p>
                     </span>
                     <span>
                         <p className='md:text-base text-sm font-medium'>{props.username} {'<>'} {props.name}</p>
