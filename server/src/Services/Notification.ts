feeabcf238af2815f68fbc577def1ee095eebbd2
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-03-08 19:53:04 +0530

    Final Take

--- server/src/Services/Notification.ts
+++ server/src/Services/Notification.ts
@@ -13,7 +13,7 @@ let reminderTime
 let sendToTime
 
 const sortTodaysMeetings = (array: any[]) => {
-    console.log(array);
+    console.log("array", array);
 
     array?.map((data) => {
         let currentTimeArray = data.scheduled_time.split(':')
@@ -24,71 +24,83 @@ const sortTodaysMeetings = (array: any[]) => {
 
     if (todayDate && meetings.length > 0) {
         // const date = new Date(Number(todayDate[2]), Number(todayDate[1]) - 1, Number(todayDate[0]), Number(String(meetings[0].scheduled_time).slice(0, 2)), Number(String(meetings[0].scheduled_time).slice(2, 4)), 0)
-        time = Number(`{${date.getHours()}${date.getMinutes()}`)
-
-        schedule.scheduleJob('* /1 * * * *', () => {
+        schedule.scheduleJob('0 * * * * *', () => {
+            console.log("enter scheduler");
+            time = Number(`${new Date().getHours()}${new Date().getMinutes()}`)
             let updateTime
             let sendTime
             for (let i = 0; i < meetings.length; i++) {
-                if (time + 15 === meetings[i].scheduled_time) {
+                console.log(time, meetings[i].scheduled_time);
+                if (time + 15 == meetings[i].scheduled_time || Number(`${new Date().getHours()}${new Date().getMinutes() + 15}`) == meetings[i].scheduled_time) {
+                    console.log("enter scheduler TT");
                     sendUpdates.push(meetings[i])
                     updateTime = meetings[i].scheduled_time - 15
                     sendTime = meetings[i].scheduled_time
                 }
             }
-            reminderTime = new Date(Number(date.getFullYear()), Number(date.getMonth()) + 1, Number(date.getDay()), Number(String(updateTime).slice(0, 2)), Number(String(updateTime).slice(2, 4)), 0)
-            sendToTime = new Date(Number(date.getFullYear()), Number(date.getMonth()) + 1, Number(date.getDay()), Number(String(sendTime).slice(0, 2)), Number(String(sendTime).slice(2, 4)), 0)
-
-            schedule.scheduleJob(reminderTime, () => {
-                sendUpdates?.map(async (data) => {
-                    const host_email_message = {
-                        from: process.env.EMAIL_USER,
-                        to: data.host_email,
-                        subject: `Reminder: ${data.event_name} with ${data.user_email}`,
-                        text: `Hi, This is a friendly reminder for the ${data.event_name} with ${data.user_email} at ${data.scheduled_time}`
-                    }
-                    await sendEmail(host_email_message);
-                    const user_email_message = {
-                        from: process.env.EMAIL_USER,
-                        to: data.user_email,
-                        subject: `Reminder: ${data.event_name} with ${data.host_email}`,
-                        text: `Hi, This is a friendly reminder for the ${data.event_name} with ${data.host_email} at ${data.scheduled_time}`
-                    }
-                    await sendEmail(user_email_message);
-                })
-            })
-
-            schedule.scheduleJob(sendToTime, () => {
-                sendUpdates?.map(async (data) => {
-                    const host_email_message = {
-                        from: process.env.EMAIL_USER,
-                        to: data.host_email,
-                        subject: `Reminder: ${data.event_name} with ${data.user_email}`,
-                        text: `Hi, This is a friendly reminder for the ${data.event_name} with ${data.user_email} at ${data.scheduled_time}`
-                    }
-                    await sendEmail(host_email_message);
-                    const user_email_message = {
-                        from: process.env.EMAIL_USER,
-                        to: data.user_email,
-                        subject: `Reminder: ${data.event_name} with ${data.host_email}`,
-                        text: `Hi, This is a friendly reminder for the ${data.event_name} with ${data.host_email} at ${data.scheduled_time}`
-                    }
-                    await sendEmail(user_email_message);
-                    sendUpdates.pop()
-                })
-                time = Number(`{${date.getHours()}${date.getMinutes()}`)
-            })
-
-            schedule.scheduleJob(date, () => {
-                console.log("task executed");
-            })
+            if (updateTime && sendTime) {
+                reminderTime = new Date(Number(date.getFullYear()), Number(date.getMonth()) + 1, Number(date.getDay()), Number(String(updateTime).slice(0, 2)), Number(String(updateTime).slice(2, 4)), 0)
+                sendToTime = new Date(Number(date.getFullYear()), Number(date.getMonth()) + 1, Number(date.getDay()), Number(String(sendTime).slice(0, 2)), Number(String(sendTime).slice(2, 4)), 0)
+                sendNotification(reminderTime, sendToTime)
+            }
         })
     } else {
         console.log("No meetings today");
     }
 }
 
-export const Notification = async () => {
+const sendNotification = (reminderTime: Date, sendToTime: Date) => {
+    schedule.scheduleJob(reminderTime, () => {
+        sendUpdates?.map(async (data) => {
+            const host_email_message = {
+                from: process.env.EMAIL_USER,
+                to: data.host_email,
+                subject: `Reminder: ${data.event_name} with ${data.user_email}`,
+                text: `Hi, This is a friendly reminder for the ${data.event_name} with ${data.user_email} at ${data.scheduled_time}`
+            }
+            await sendEmail(host_email_message);
+            const user_email_message = {
+                from: process.env.EMAIL_USER,
+                to: data.user_email,
+                subject: `Reminder: ${data.event_name} with ${data.host_email}`,
+                text: `Hi, This is a friendly reminder for the ${data.event_name} with ${data.host_email} at ${data.scheduled_time}`
+            }
+            await sendEmail(user_email_message);
+        })
+    })
+
+    schedule.scheduleJob(sendToTime, () => {
+        sendUpdates?.map(async (data) => {
+            const host_email_message = {
+                from: process.env.EMAIL_USER,
+                to: data.host_email,
+                subject: `Reminder: ${data.event_name} with ${data.user_email}`,
+                text: `Hi, This is a friendly reminder for the ${data.event_name} with ${data.user_email} at ${data.scheduled_time}`
+            }
+            await sendEmail(host_email_message);
+            const user_email_message = {
+                from: process.env.EMAIL_USER,
+                to: data.user_email,
+                subject: `Reminder: ${data.event_name} with ${data.host_email}`,
+                text: `Hi, This is a friendly reminder for the ${data.event_name} with ${data.host_email} at ${data.scheduled_time}`
+            }
+            await sendEmail(user_email_message);
+            sendUpdates.pop()
+        })
+        time = Number(`{${date.getHours()}${date.getMinutes()}`)
+    })
+}
+
+export const cleanUp = async () => {
+    try {
+        await client.expireat('meetings:1', 5)
+        Notification()
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const Notification = async () => {
     console.log("enter")
     try {
         const cacheValue = await client.get('meetings:1')
@@ -97,9 +109,10 @@ export const Notification = async () => {
             if (cacheData.length > 0) {
                 cacheData?.map((data: any) => {
                     console.log("dat", data.scheduled_date);
-                    const currentDataTime = data.scheduled_date
-                    todayDate == currentDataTime ?
-                        todayMeetings.push(data) : null
+                    const currentDataTime = new Date(data.scheduled_date)
+                    if (currentDataTime.getTime() == todayDate.getTime()) {
+                        todayMeetings.push(data)
+                    }
                 })
                 console.log("td", todayMeetings);
                 sortTodaysMeetings(todayMeetings)
@@ -111,6 +124,7 @@ export const Notification = async () => {
             const response = await pool.query('SELECT * from Meetings');
             if (response.rows.length > 0) {
                 await client.set('meetings:1', JSON.stringify(response.rows))
+                Notification()
             } else {
                 console.log("No Meetings data fetched");
             }
